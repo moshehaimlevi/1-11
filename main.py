f2c9023c9456aaaeba24cd01be0fda039290814c
moshehaimlevi <modernguy25@gmail.com>
2024-11-01 19:50:31 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from my_func import print_stars
+import my_func
+
+help(my_func)
+help(print_stars)
+
+
+for _ in range(5):
+    print_stars()
---
+++ my_func.py
@@ -0,0 +1,11 @@
+def print_stars():
+    """print'*******'"""
+    print('********')
+
+
+if __name__ == '__main__':
+    print('correct')
+    print('correct')
+    print('correct')
+    print('correct')
+    print('correct')
